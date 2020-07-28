874de07ba71006c5a4b36d6921402c61fa8d8d85
Weird Dan <daniel.grenehed97@gmail.com>
2020-07-28 21:24:28 +0200

    added circuit explination

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ A footswitcher to midi controller arduino program
 I built a little device that takes input from jack connected footswitches and pedals and sends it as midi messages over usb.
 
 It is based on the ardunio Leonardo for its native usb functionality!
+
+## Components
+Component list:
+
+* Arduino Leonardo (since it is capable as running as a usb midi device)
+* TRS connectors (3)
+* Two way switch
+* USB 2.0 type B connector
+* Micro USB 2.0 cable with cut opened
+* Wire
+
+## Setup
+To interface with device there is a USB type B connector is connected to the loose wires of the micro usb cable. The micro usb connector is connected to the Arduino.
+
+From the Arduino, ground is running to all the TRS jack sleeves in series.
+
+The TRS connectors are connected to digital pins 1 -> 6 on the Arduino, with the TRS tips connected at 1, 3 and 5 and the TRS rings connected at ping 2, 4 and 6. Analog in A0 is also connected to TRS 3 ring.
+
+The switch is connecting digital pin 7 to 5v when in the "off" position, and 5v to TRS 3 tip (same as digital pin 6).
+
+The TRS jacks are used to plug in external switches, allowing up to 6 footswitches connected at once, or 4 switches and 1 expression pedal.
+
+## Building a switch-box
+A connected switch-box would be built by simply connecting ground(sleeve) to the input of the two footswitches and the other (ring and tip) to the output of the footswitches(Could be any switch connecting/disconnecting two pins).
--- midiUsbSwitcher/midiUsbSwitcher.ino
+++ midiUsbSwitcher/midiUsbSwitcher.ino
@@ -4,27 +4,36 @@
 
 #define DEBUG
 
+/*
 #define switch_1 1
 #define switch_2 2
 #define switch_3 3
 #define switch_4 4
 #define switch_5 5
-#define switch_6 5
-#define isNEx 7
+#define switch_6 5*/
+#define num_switches 6
 
-#define ExP A0
+int switches[num_switches] = {1, 2, 3, 4, 5, 5} // switch pins, two per tele-jack
 
+#define isNEx 7 // on/off switch pin
+
+#define ExP A0 // expression pedal on analog 0
+
+/*
 int lastSwitchState_1 = 0; // last switch state for switch 1
 int lastSwitchState_2 = 0;
 int lastSwitchState_3 = 0;
 int lastSwitchState_4 = 0;
 int lastSwitchState_5 = 0;
-int lastSwitchState_6 = 0;
+int lastSwitchState_6 = 0;*/
+int switchStates[num_switches]; // store last read value of pins
 
-int pedalValue = 0;
 int lastPedalValue = 0;
-int tempAnalogValue = 0;
 int largestPedalValue = 0;
+
+//int pedalValue = 0;
+//int tempAnalogValue = 0;
+
 
 
 // the setup routine runs once when you press reset:
@@ -33,54 +42,86 @@ void setup() {
   Serial.begin(9600);
 
   // setup switches as inputs
+  /*
   pinMode(switch_1, INPUT_PULLUP);
   pinMode(switch_2, INPUT_PULLUP);
   pinMode(switch_3, INPUT_PULLUP);
   pinMode(switch_4, INPUT_PULLUP);
   pinMode(switch_5, INPUT_PULLUP);
   pinMode(switch_6, INPUT_PULLUP);
+  */
+  resetSwitchStates();
+  setSwitchInputs();
 
   // expression on/off switch
   pinMode(isNEx, INPUT_PULLUP);
 
 }
 
+void resetSwitchStates() {
+  for (int i = 0; i < num_switches; i++) {
+    switchStates[i] = 0;
+  }
+}
 
+void setSwitchInputs() {
+  for (int i = 0; i < num_switches; i++) {
+    pinMode(switches[i], INPUT_PULLUP);
+  }
+}
+
+
+// fix loop please
 void loop() {
 
+  /*
+
+  should be as simple as:
+
+  for (int i = 0; i < num_switches; i++) {
+    handleSwitch(i)
+  }
+
+
+  */
+
   MidiMessage msg = {0, 1, 3, 1};
-  int tps = digitalRead(switch_1);
+  int temp_switch_value = digitalRead(switches[0]);
 
-  handleSwitch(switch_1, tps, lastSwitchState_1, msg);
+  handleSwitch(switches[0], temp_switch_value, switchStates[0], msg);
 
-  tps = digitalRead(switch_2);
+  temp_switch_value = digitalRead(switches[1];
   msg.Data_1 = 9;
-  handleSwitch(switch_2, tps, lastSwitchState_2, msg);
+  handleSwitch(switches[1], temp_switch_value, switchStates[1], msg);
 
-  tps = digitalRead(switch_3);
+  temp_switch_value = digitalRead(switches[2]);
   msg.Data_1 = 3;
-  msg.Data_2 = tps;
-  handleSwitch(switch_3, tps, lastSwitchState_3, msg);
+  msg.Data_2 = temp_switch_value;
+  handleSwitch(switches[2], temp_switch_value, switchStates[2], msg);
 
-  tps = digitalRead(switch_4);
-  msg.Data_2 = tps;
-  handleSwitch(switch_4, tps, lastSwitchState_4, msg);
+  temp_switch_value = digitalRead(switches[3]);
+  msg.Data_2 = temp_switch_value;
+  handleSwitch(switches[3], temp_switch_value, switchStates[3], msg);
 
-  tps = digitalRead(isNEx);
+  temp_switch_value = digitalRead(isNEx); // switch in pedal on/off
+  /*
+    if switch is off, then handle input 5 and 6 as a expression pedal
+    if switch is on, then handle inpu 5 and 6 as seperate switches
+  */
 
 
-  if (tps == 0) {
+  if (temp_switch_value == 0) {
     handlePedal();
   } else {
-    tps = digitalRead(switch_5);
+    temp_switch_value = digitalRead(switches[4]);
     msg.Data_1 = 14;
-    msg.Data_2 = tps;
-    handleSwitch(switch_5, tps, lastSwitchState_5, msg);
+    msg.Data_2 = temp_switch_value;
+    handleSwitch(switches[4], temp_switch_value, switchStates[4], msg);
 
-    tps = digitalRead(switch_6);
+    temp_switch_value = digitalRead(switches[5]);
     msg.Data_1 = 15;
-    msg.Data_2 = tps;
-    handleSwitch(switch_6, tps, lastSwitchState_6, msg);
+    msg.Data_2 = temp_switch_value;
+    handleSwitch(switches[5], temp_switch_value, switchStates[5], msg);
   }
 
   delay(2);
@@ -92,8 +133,8 @@ void loop() {
   Todo: Generalize function!
 */
 bool handlePedal() {
-  pedalValue = 0;
-  tempAnalogValue = analogRead(ExP);
+  //pedalValue = 0;
+  int tempAnalogValue = analogRead(ExP);
 
   if (tempAnalogValue > largestPedalValue) {
     largestPedalValue = tempAnalogValue;
@@ -126,7 +167,7 @@ bool handlePedal() {
 bool handleSwitch(int switchnr, int state, int &lastState, MidiMessage msg) {
   if (state != lastState) {
 
-    switch (msg.Type) {
+    switch (msg.Type) { // see midiMessage.h for types
       case (0):
       controlChange(msg.Channel, msg.Data_1, msg.Data_2);
       break;
