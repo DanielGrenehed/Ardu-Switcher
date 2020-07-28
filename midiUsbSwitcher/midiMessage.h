0d76e8db098d88e4e2d6e6cec11a54b3f6dc6bef
Weird Dan <daniel.grenehed97@gmail.com>
2020-07-28 23:30:47 +0200

    refactored main switcher code

--- midiUsbSwitcher/midiMessage.h
+++ midiUsbSwitcher/midiMessage.h
@@ -8,10 +8,27 @@ enum MidiMessageType {
   Control = 0, NoteOn = 1, NoteOff = 2
 };
 
+byte typeBytes[] = {0xB0, 0x90, 0x80}; // type of message in byte representation
+byte channelBytes[] = {0x0B, 0x09, 0x08}; // byte for channel or operation
+
+/*
+  A simpler representation of a midi message,
+*/
+struct MidiMessage {
+  MidiMessageType Type;
+  byte Channel;
+  byte Data_1;
+  byte Data_2;
+};
+
+/*
+  returns 1 if message type exists and is fully implemented
+  returns 0 on fail.
+*/
 int validMidiMessageType(int msgType) {
   int valid = 0;
 
-  switch(msgType) {
+  switch(msgType) { // case for all fully implemented midi message types
     case MidiMessageType.Control:
     case MidiMessageType.NoteOn:
     case MidiMessageType.NoteOff:
@@ -21,25 +38,29 @@ int validMidiMessageType(int msgType) {
   return valid;
 }
 
-struct MidiMessage {
-  MidiMessageType Type;
-  byte Channel;
-  byte Data_1;
-  byte Data_2;
-};
+/*
 
-byte typeBytes[] = {0xB0, 0x90, 0x80};
-byte channelBytes[] = {0x0B, 0x09, 0x08};
+  Convert MidiMessage to packet and sends midi
 
+  Todo:
+    add send to other midi destinations.
+*/
 void sendMidi(MidiMessage msg) {
-  sendMidiUsb(createMidiPacket(msg))
+  midiEventPacket_t packet = createMidiPacket(msg);
+  sendMidiUsb(packet);
 }
 
+/*
+  Converts a MidiMessage to a packet
+*/
 midiEventPacket_t createMidiPacket(MidiMessage msg) {
   midiEventPacket_t packet = {typeBytes[msg.Type], channelBytes[msg.Type] | msg.Channel, msg.Data_1, msg.Data_2};
   return packet
 }
 
+/*
+  Send midi packet over usb.
+*/
 void sendMidiUsb(midiEventPacket_t packet) {
   MidiUSB.sendMIDI(packet);
   MidiUSB.flush();
--- midiUsbSwitcher/midiUsbSwitcher.ino
+++ midiUsbSwitcher/midiUsbSwitcher.ino
@@ -6,31 +6,34 @@
 
 /*
 
-  Todo: move midi message creation to when it should be sent, to not have a kind of
-  prototype message floating around the whole loop
-
-
-
-  Move all switch handling to the handleSwitch function, removing the need to pass
-  midi message, digital read value and switch state. Then maby subdevide that function into
-  smaller functions
-
 */
-
 
 #define num_switches 6
 #define ExP A0 // expression pedal on analog 0
 #define isNEx 7 // on/off switch pin
 
-int switches[num_switches] = {1, 2, 3, 4, 5, 6} // switch pins, two per tele-jack
+int switches[num_switches] = {1, 2, 3, 4, 5, 6}; // switch pins, two per tele-jack
 
 int switchStates[num_switches]; // store last read value of pins
+
+/*
+
+  Mapping switches to midi messages
+
+*/
+int defaultTypes[] = {0, 0, 0, 0, 0, 0}; // default midi message types
+#define m_ch 1 // default midi channel
+int defaultNotes[] = {3, 9, 3, 3, 14, 15}; // defaults for data_1
+int defaultValues[] = {1, 1, -1, -1, -1, -1}; // defaults for data_2
+/*
+  If value is negative, use an variable, in this case what digitalRead gave
+*/
 
 int lastPedalValue = 0; // have the value changed
 int largestPedalValue = 0; // max value for expression pedal, used to map to a 0->127 value
 
 
-// the setup routine runs once when you press reset:
+/**/
 void setup() {
 
   Serial.begin(9600);
@@ -44,12 +47,14 @@ void setup() {
 
 }
 
+/**/
 void resetSwitchStates() {
   for (int i = 0; i < num_switches; i++) {
     switchStates[i] = 0;
   }
 }
 
+/**/
 void setSwitchInputs() {
   for (int i = 0; i < num_switches; i++) {
     pinMode(switches[i], INPUT_PULLUP);
@@ -57,57 +62,19 @@ void setSwitchInputs() {
 }
 
 
-// fix loop please
+/*
+
+*/
 void loop() {
 
-  /*
-
-  should be as simple as:
-
   for (int i = 0; i < num_switches; i++) {
-    handleSwitch(i)
-  }
 
+    handleSwitch(i);
 
-  */
-
-  MidiMessage msg = {0, 1, 3, 1}; // {midi_message_type, midi_channel, data_1, data_2 }
-  int temp_switch_value = digitalRead(switches[0]);
-
-  handleSwitch(switches[0], temp_switch_value, switchStates[0], msg);
-
-  temp_switch_value = digitalRead(switches[1];
-  msg.Data_1 = 9; // what is 9?
-  handleSwitch(switches[1], temp_switch_value, switchStates[1], msg);
-
-  temp_switch_value = digitalRead(switches[2]);
-  msg.Data_1 = 3; // what is 3?
-  msg.Data_2 = temp_switch_value; // what is temp_switch_value? 0 or 1, 0 -> 127?
-  handleSwitch(switches[2], temp_switch_value, switchStates[2], msg);
-
-  temp_switch_value = digitalRead(switches[3]);
-  msg.Data_2 = temp_switch_value;
-  handleSwitch(switches[3], temp_switch_value, switchStates[3], msg);
-
-  temp_switch_value = digitalRead(isNEx); // switch in pedal on/off
-  /*
-    if switch is off, then handle input 5 and 6 as a expression pedal
-    if switch is on, then handle inpu 5 and 6 as seperate switches
-  */
-
-
-  if (temp_switch_value == 0) { // make more explicit code and clearer variable names, temp_switch_value is overused
-    handlePedal();
-  } else {
-    temp_switch_value = digitalRead(switches[4]);
-    msg.Data_1 = 14;
-    msg.Data_2 = temp_switch_value;
-    handleSwitch(switches[4], temp_switch_value, switchStates[4], msg);
-
-    temp_switch_value = digitalRead(switches[5]);
-    msg.Data_1 = 15;
-    msg.Data_2 = temp_switch_value;
-    handleSwitch(switches[5], temp_switch_value, switchStates[5], msg);
+    if (i == num_switches -3 && digitalRead(isNEx) == 0) {
+      handlePedal();
+      break; // its set to use expression instead of last two switches
+    }
   }
 
   delay(2);
@@ -115,8 +82,6 @@ void loop() {
 
 /*
 
-
-  Todo: Generalize function!
 */
 bool handlePedal() {
   int value = mapPedalToByte(analogRead(ExP));
@@ -126,11 +91,15 @@ bool handlePedal() {
   #endif
 
   if (value != lastPedalValue) {
-    sendMidi({0, 1, 11, value}); // send control change
+    sendMidi({0, m_ch, 11, value}); // send control change
     lastPedalValue = value;
   }
 }
 
+/*
+
+
+*/
 int mapPedalToByte(int value) {
   if (value > largestPedalValue) largestPedalValue = value; // set record high value to constrain
   value = constrain(value, 0, largestPedalValue); // constrain to highest value recorded
@@ -143,10 +112,7 @@ int mapPedalToByte(int value) {
 */
 bool handleSwitch(int switchnr, int state, int &lastState, MidiMessage msg) {
   if (state != lastState) {
-    if (validMidiMessageType(msg.Type)) {
-      sendMidi(msg);
-    }
-
+    if (validMidiMessageType(msg.Type)) sendMidi(msg);
     lastState = state;
   }
 }
